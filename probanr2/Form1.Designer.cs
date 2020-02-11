3010e5fc4e185c84309adc58395a61ef663b2b8a
Wiilee <kamilland123@gmail.com>
2020-02-11 15:25:40 +0000

    First commit

---
+++ probanr2/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace probanr2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdSelect = new System.Windows.Forms.Button();
+            this.cmbEmp = new System.Windows.Forms.ComboBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // cmdSelect
+            // 
+            this.cmdSelect.Location = new System.Drawing.Point(278, 175);
+            this.cmdSelect.Name = "cmdSelect";
+            this.cmdSelect.Size = new System.Drawing.Size(272, 23);
+            this.cmdSelect.TabIndex = 0;
+            this.cmdSelect.Text = "Open Excel File";
+            this.cmdSelect.UseVisualStyleBackColor = true;
+            this.cmdSelect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cmbEmp
+            // 
+            this.cmbEmp.FormattingEnabled = true;
+            this.cmbEmp.Location = new System.Drawing.Point(346, 238);
+            this.cmbEmp.Name = "cmbEmp";
+            this.cmbEmp.Size = new System.Drawing.Size(121, 21);
+            this.cmbEmp.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbEmp);
+            this.Controls.Add(this.cmdSelect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdSelect;
+        private System.Windows.Forms.ComboBox cmbEmp;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ probanr2/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+
+namespace probanr2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Excel.Application xlApp = new Excel.Application();
+        Excel.Workbook xlWorkBook;
+        Excel.Worksheet xlWorkSheet;
+
+        string sFileName;
+        int iRow, iCol = 2;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+            openFileDialog1.Title = "Excel File to Edit";
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "Excel File|*.xlsx;*.xls";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                sFileName = openFileDialog1.FileName;
+
+                if (sFileName.Trim() != "")
+                {
+                    readExcel(sFileName);
+                }
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void readExcel(string sFile)
+        {
+            xlApp = new Excel.Application();
+            xlWorkBook = xlApp.Workbooks.Open(sFile);           // WORKBOOK TO OPEN THE EXCEL FILE.
+            xlWorkSheet = xlWorkBook.Worksheets["Sheet1"];      // NAME OF THE SHEET.
+
+            for (iRow = 1; iRow <= xlWorkSheet.Rows.Count; iRow++)  // START FROM THE SECOND ROW.
+            {
+                if (xlWorkSheet.Cells[iRow, 1].value == null)
+                {
+                    break;      // BREAK LOOP.
+                }
+                else
+                {               // POPULATE COMBO BOX.
+                    cmbEmp.Items.Add(xlWorkSheet.Cells[iRow, 1].value);
+                }
+            }
+
+            xlWorkBook.Close();
+            xlApp.Quit();
+        }
+    }
+}
