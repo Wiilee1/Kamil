0cf95e5aa38b17f20e3c6b1f783f4506962d47bc
Wiilee <kamilland123@gmail.com>
2020-02-13 11:03:44 +0000

    Sorting columns

--- probanr2/Form1.cs
+++ probanr2/Form1.cs
@@ -9,12 +9,15 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.IO;
+using System.Collections;
 
 namespace probanr2
 {
     public partial class Form1 : System.Windows.Forms.Form
     {
         List<Customer> customers = new List<Customer>();
+        private int sortColumn = -1;
+
         public Form1()
         {
             InitializeComponent();
@@ -32,10 +35,10 @@ namespace probanr2
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {            
+        {
             openFileDialog1.Title = "Excel File to Edit";
             openFileDialog1.FileName = "";
-            openFileDialog1.Filter = "Excel File|*.xlsx;*.xls";
+            openFileDialog1.Filter = "Excel File|*.xlsx;*.xls;*xlsm";
 
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
@@ -63,35 +66,81 @@ namespace probanr2
 
         }
 
+        private void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            // Determine whether the column is the same as the last column clicked.
+            if (e.Column != sortColumn)
+            {
+                // Set the sort column to the new column.
+                sortColumn = e.Column;
+                // Set the sort order to ascending by default.
+                listView1.Sorting = SortOrder.Ascending;
+            }
+            else
+            {
+                // Determine what the last sort order was and change it.
+                if (listView1.Sorting == SortOrder.Ascending)
+                    listView1.Sorting = SortOrder.Descending;
+                else
+                    listView1.Sorting = SortOrder.Ascending;
+            }
+
+            // Call the sort method to manually sort.
+            listView1.Sort();
+            // Set the ListViewItemSorter property to a new ListViewItemComparer
+            // object.
+            this.listView1.ListViewItemSorter = new ListViewItemComparer(e.Column, listView1.Sorting);
+        }
+
         private void readExcel(string sFile)
         {
             xlApp = new Excel.Application();
             xlWorkBook = xlApp.Workbooks.Open(sFile);           // WORKBOOK TO OPEN THE EXCEL FILE.
-            xlWorkSheet = xlWorkBook.Worksheets[2];      // NAME OF THE SHEET.
 
-            string name, address, email, county;
-            double phone, reference;
+            for (int i = 1; i < 32; i++)
+            {
+                xlWorkSheet = xlWorkBook.Worksheets[i];
 
-            name = xlWorkSheet.Cells[11, 2].value;
-            address = xlWorkSheet.Cells[11, 4].value;
-            email = xlWorkSheet.Cells[11, 8].value;
-            county = xlWorkSheet.Cells[39, 7].value;
-            phone = xlWorkSheet.Cells[11, 6].value;
-            string phonestring = System.Convert.ToString(phone);
-            reference = xlWorkSheet.Cells[7, 3].value;
-            string refstring = System.Convert.ToString(reference);
+                string name, address, email, county, phonestring;
+                double phone, reference;
 
-            ListViewItem item = new ListViewItem(xlWorkSheet.Cells[11, 2].value);
-            item.SubItems.Add(address);
-            item.SubItems.Add(email);
-            item.SubItems.Add(county);
-            item.SubItems.Add(phonestring);
-            item.SubItems.Add(refstring);
-            listView1.Items.Add(item);
-            customers.Add(new Customer(name, address, email, county, phone, reference));
+                name = xlWorkSheet.Cells[11, 2].value;
+                address = xlWorkSheet.Cells[11, 4].value;
+                email = xlWorkSheet.Cells[11, 8].value;
+                county = xlWorkSheet.Cells[39, 7].value;
+                if (xlWorkSheet.Cells[11, 6].value != null)
+                {
+                    phone = xlWorkSheet.Cells[11, 6].value;
+                    phonestring = System.Convert.ToString(phone);
+                }
+                else
+                {
+                    phone = 0;
+                    phonestring = "";
+                }
+
+                reference = xlWorkSheet.Cells[7, 3].value;
+                string refstring = System.Convert.ToString(reference);
+
+                ListViewItem item = new ListViewItem(xlWorkSheet.Cells[11, 2].value);
+                item.SubItems.Add(address);
+                item.SubItems.Add(email);
+                item.SubItems.Add(county);
+                item.SubItems.Add(phonestring);
+                item.SubItems.Add(refstring);
+                listView1.Items.Add(item);
+                customers.Add(new Customer(name, address, email, county, phone, reference));
+            }      // NAME OF THE SHEET.
+
+
 
             xlWorkBook.Close();
             xlApp.Quit();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
 
 
